ce57e05f38ffc28d5fc1501e3cbc84740ada0a3e
mgoria <goria.marcel@gmail.com>
2015-10-28 12:34:54 +0200

    add common module for unit tests

--- src/deep-asset/test/Asset.js
+++ src/deep-asset/test/Asset.js
@@ -3,131 +3,48 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import {Asset} from '../lib.compiled/Asset';
-import Kernel from 'deep-kernel';
+import Asset from '../lib.compiled/Asset';
 import Instance from '../node_modules/deep-kernel/lib.compiled/Microservice/Instance';
+import KernelFactory from "./common/KernelFactory";
 
 chai.use(sinonChai);
 
 suite('Asset', function() {
-  let asset = new Asset();
-
-  test('Class Asset exists in Asset', function() {
-    chai.expect(typeof Asset).to.equal('function');
-  });
-
-  test('Check boot() method  for !kernel.isFrontend', function() {
-    let kernel = {isFrontend: false};
+  KernelFactory.create({'Asset': Asset}, (frontendKernel, backendKernel) => {
+    let assetService = new Asset();
     let spyCallback = sinon.spy();
-    asset.boot(kernel, spyCallback);
-    chai.expect(spyCallback).to.have.been.calledWith();
-  });
 
-  test('Check boot() method  for kernel.isFrontend', function() {
-    let spyCallback = sinon.spy();
-    let error = null;
-    let kernelMock = {
-      isFrontend: true,
-      microservices: {
-        'deep.test': {
-          identifier: 'deep.test',
-          rawResources: {
-            test: {
-              create: {
-                description: 'Lambda for creating test',
-                type: 'lambda',
-                methods: [
-                  'POST',
-                ],
-                source: 'src/Test/Create',
-              },
-              retrieve: {
-                description: 'Retrieves test',
-                type: 'lambda',
-                methods: ['GET'],
-                source: 'src/Test/Retrieve',
-              },
-              delete: {
-                description: 'Lambda for deleting test',
-                type: 'lambda',
-                methods: ['DELETE'],
-                source: 'src/Test/Delete',
-              },
-              update: {
-                description: 'Update test',
-                type: 'lambda',
-                methods: ['PUT'],
-                source: 'src/Test/Update',
-              },
-            },
-          },
-          isRoot: true,
-        },
-        'deep.second.test': {
-          identifier: 'deep.second.test',
-          rawResources: {
-            test: {
-              create: {
-                description: 'Lambda for creating secondTest',
-                type: 'lambda',
-                methods: [
-                  'POST',
-                ],
-                source: 'src/secondTest/Create',
-              },
-              retrieve: {
-                description: 'Retrieves secondTest',
-                type: 'lambda',
-                methods: ['GET'],
-                source: 'src/secondTest/Retrieve',
-              },
-              delete: {
-                description: 'Lambda for deleting secondTest',
-                type: 'lambda',
-                methods: ['DELETE'],
-                source: 'src/secondTest/Delete',
-              },
-              update: {
-                description: 'Update secondTest',
-                type: 'lambda',
-                methods: ['PUT'],
-                source: 'src/secondTest/Update',
-              },
-            },
-          },
-          isRoot: true,
-        },
-      },
-      container: {
-        addParameter: function() {
-          return 'parameter added';
-        },
-      },
-    };
+    test('Class Asset exists in Asset', function() {
+      chai.expect(typeof Asset).to.equal('function');
+    });
 
-    try {
-      asset.boot(kernelMock, spyCallback);
-    } catch (e) {
-      error = e;
-    }
+    test('Check boot() method for !kernel.isFrontend', function() {
+      assetService.boot(backendKernel, spyCallback);
+      chai.expect(spyCallback).to.have.been.calledWith();
+    });
 
-    chai.expect(error).to.be.equal(null);
-    chai.expect(spyCallback).to.have.been.calledWith();
+    test('Check boot() method  for kernel.isFrontend', function() {
+      let error = null;
 
-  });
+      try {
+        assetService.boot(frontendKernel, spyCallback);
+      } catch (e) {
+        error = e;
+      }
 
-  test('Check locate() method  returns valid string', function() {
-    let deepServices = new Kernel.MicroserviceInjectable();
-    chai.assert.instanceOf(deepServices, Kernel.MicroserviceInjectable, 'deepServices is an instance of MicroserviceInjectable');
-    let error = null;
-    let kernel = null;
-    let actualResult = null;
-    try {
-      kernel = new Kernel(deepServices, Kernel.FRONTEND_CONTEXT);
-      actualResult = asset.locate({});
-    } catch (e) {
-      error = e;
-    }
+      // @todo - check if FRONTEND_BOOTSTRAP_VECTOR was created instead of checking for exceptions
+      chai.expect(error).to.be.equal(null);
+      chai.expect(spyCallback).to.have.been.calledWith();
 
+    });
+
+    test('Check locate() method returns valid string', function() {
+      assetService.boot(frontendKernel, spyCallback);
+
+      let expectedResult = "hello.world.example/bootstrap.js";
+      let actualResult = assetService.locate("@hello.world.example:bootstrap.js");
+
+      chai.expect(actualResult).to.be.equal(expectedResult);
+    });
   });
 });
---
+++ src/deep-asset/test/common/KernelFactory.js
@@ -0,0 +1,20 @@
+import backendConfig from "./backend-cfg-json";
+import frontendConfig from "./frontent-cfg-json";
+import Kernel from 'deep-kernel';
+
+export default {
+  /**
+   * @param {Object} services
+   * @param {Function} callback
+   */
+  create: function(services, callback) {
+    let frontendInstance = new Kernel(services, Kernel.FRONTEND_CONTEXT);
+    let backendInstance = new Kernel(services, Kernel.BACKEND_CONTEXT);
+
+    return frontendInstance.load(frontendConfig, (frontendKernel) => {
+      backendInstance.load(backendConfig, (backendKernel) => {
+        callback(frontendKernel, backendKernel);
+      });
+    });
+  }
+}
