94d9888536b12f86f8fdb52ac97219633face217
AlexanderC <alexander.moldova@gmail.com>
2015-11-11 17:37:22 +0200

    Add tmp tests trace

--- src/deep-log/test/Driver/ConsoleDriver.js
+++ src/deep-log/test/Driver/ConsoleDriver.js
@@ -23,8 +23,8 @@ suite('Driver/ConsoleDriver', function() {
       error = e;
     }
 
+    if (error) console.log(error, error.stack);//@todo:remove after debugging!!!
+    
     chai.expect(error).to.be.equal(null);
-
-    console.log(error, error.stack);//@todo:remove after debugging!!!
   });
 });
