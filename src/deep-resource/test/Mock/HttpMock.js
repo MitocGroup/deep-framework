535de1da3e8612ca9fb4de27763d2c17bd33b2b7
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-11-12 12:19:36 +0200

    Refactor LocalRequest

---
+++ src/deep-resource/test/Mock/HttpMock.js
@@ -0,0 +1,339 @@
+'use strict';
+
+import Http from 'superagent';
+
+export class HttpMock extends Http {
+  constructor(...args) {
+    super(args);
+    console.log('Http: ', Http);
+    this._methodsBehavior = new Map();
+    this.enableNoResultMode();
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  post(endpoint, callback) {
+    switch (this._methodsBehavior.get('post')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  get(endpoint, callback) {
+    switch (this._methodsBehavior.get('get')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {*} data
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  put(endpoint, data, callback) {
+    switch (this._methodsBehavior.get('put')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {String} id
+   * @param {*} data
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  patch(endpoint, id, data, callback) {
+    switch (this._methodsBehavior.get('patch')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {String} id
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  delete(endpoint, id,  callback) {
+    switch (this._methodsBehavior.get('delete')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {String} endpoint
+   * @param {*} data
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  options(endpoint, data, callback) {
+    switch (this._methodsBehavior.get('options')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {*} data
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  send(data, callback) {
+    switch (this._methodsBehavior.get('send')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * @param {Function} callback
+   * @returns {HttpMock}
+   */
+  end(callback) {
+    switch (this._methodsBehavior.get('end')) {
+      case HttpMock.NO_RESULT_MODE:
+        callback(null, null);
+        break;
+
+      case HttpMock.FAILURE_MODE:
+        callback(HttpMock.ERROR, null);
+        break;
+
+      case HttpMock.DATA_MODE:
+        callback(null, HttpMock.DATA);
+        break;
+    }
+
+    return this;
+  }
+
+  /**
+   * Enables no results mode
+   * @param methods
+   */
+  enableNoResultMode() {
+    for (let method of HttpMock.METHODS) {
+      this._methodsBehavior.set(method, HttpMock.NO_RESULT_MODE);
+    }
+  }
+
+  /**
+   * Enables failure mode
+   * @param methods
+   */
+  enableFailureMode() {
+    for (let method of HttpMock.METHODS) {
+      this._methodsBehavior.set(method, HttpMock.ERROR);
+    }
+  }
+
+  /**
+   * Disable failure mode
+   * @param methods
+   */
+  disableFailureMode() {
+    for (let method of HttpMock.METHODS) {
+      this._methodsBehavior.set(method, HttpMock.DATA_MODE);
+    }
+  }
+
+  /**
+   * Enables no results mode for passed methods
+   * @param methods
+   */
+  enableNoResultModeFor(methods) {
+    for (let method of methods) {
+      if (HttpMock.METHODS.indexOf(method) < 0) {
+        continue;
+      }
+
+      this._methodsBehavior.set(method, HttpMock.NO_RESULT_MODE);
+    }
+  }
+
+  /**
+   * Enables failure mode for passed methods
+   * @param {String[]} methods
+   */
+  enableFailureModeFor(methods) {
+    for (let method of methods) {
+      if (HttpMock.METHODS.indexOf(method) < 0) {
+        continue;
+      }
+
+      this._methodsBehavior.set(method, HttpMock.FAILURE_MODE);
+    }
+  }
+
+  /**
+   * Disables failure mode for passed methods
+   * @param methods
+   */
+  disableFailureModeFor(methods) {
+    for (let method of methods) {
+      if (HttpMock.METHODS.indexOf(method) < 0) {
+        continue;
+      }
+
+      this._methodsBehavior.set(method, HttpMock.DATA_MODE);
+    }
+  }
+
+  /**
+   * @returns {number}
+   * @constructor
+   */
+  static get NO_RESULT_MODE() {
+    return 0;
+  }
+
+  /**
+   * @returns {number}
+   * @constructor
+   */
+  static get FAILURE_MODE() {
+    return 1;
+  }
+
+  /**
+   * @returns {number}
+   * @constructor
+   */
+  static get DATA_MODE() {
+    return 2;
+  }
+
+  /**
+   * @returns {string}
+   * @constructor
+   */
+  static get ERROR() {
+    return {
+      code: 500,
+      body: {
+        message: 'Internal Error',
+      },
+    };
+  }
+
+  /**
+   * @returns {string}
+   * @constructor
+   */
+  static get DATA() {
+    return {
+      code: 200,
+      body: {
+        message: 'Test message',
+      },
+    };
+  }
+
+  /**
+   * @returns {string[]}
+   * @constructor
+   */
+  static get METHODS() {
+    return [
+        'post',
+        'get',
+        'put',
+        'patch',
+        'delete',
+        'options',
+        'send',
+        'end',
+    ];
+  }
+}
--- src/deep-resource/test/Resource/LocalRequest.js
+++ src/deep-resource/test/Resource/LocalRequest.js
@@ -7,88 +7,83 @@ import {LocalRequest} from '../../lib.compiled/Resource/LocalRequest';
 import {Action} from '../../lib.compiled/Resource/Action';
 import {Resource} from '../../lib.compiled/Resource';
 import CacheMock from '../Mock/CacheMock';
+import Kernel from 'deep-kernel';
+import Cache from 'deep-cache';
+import Security from 'deep-security';
+import KernelFactory from '../common/KernelFactory';
+import backendConfig from '../common/backend-cfg-json';
+import RequireProxy from 'proxyquire';
+import Http from 'superagent';
+import {HttpMock} from '../Mock/HttpMock';
+
 
 chai.use(sinonChai);
 
 suite('Resource/LocalRequest', function() {
-  let testResources = {
-    'deep.test': {
-      test: {
-        create: {
-          description: 'Lambda for creating test',
-          type: 'lambda',
-          methods: [
-            'POST',
-          ],
-          source: 'src/Test/Create',
-        },
-        retrieve: {
-          description: 'Retrieves test',
-          type: 'lambda',
-          methods: ['GET'],
-          source: 'src/Test/Retrieve',
-        },
-        delete: {
-          description: 'Lambda for deleting test',
-          type: 'lambda',
-          methods: ['DELETE'],
-          source: 'src/Test/Delete',
-        },
-        update: {
-          description: 'Update test',
-          type: 'lambda',
-          methods: ['PUT'],
-          source: 'src/Test/Update',
-        },
-      },
-    },
-  };
-  let resource = new Resource(testResources);
-  let actionName = 'UpdateTest';
-  let type = 'lambda';
-  let methods = ['GET', 'POST'];
-  let source = 'sourceTest';
-  let region = 'us-west-2';
-  let action = new Action(resource, actionName, type, methods, source, region);
+  let backendKernelInstance = null;
+  let action = null;
+  let localRequest = null;
+  let microserviceIdentifier = 'hello.world.example';
+  let resourceName = 'sample';
+  let actionName = 'say-hello';
   let payload = '{"body":"bodyData"}';
-  let method = 'method';
+  let method = 'POST';
 
   test('Class LocalRequest exists in Resource/LocalRequest', function() {
     chai.expect(typeof LocalRequest).to.equal('function');
+  });
+
+  test('Load Kernel by using Kernel.load()', function(done) {
+    let callback = (backendKernel) => {
+      chai.assert.instanceOf(
+        backendKernel, Kernel, 'backendKernel is an instance of Kernel');
+      backendKernelInstance = backendKernel;
+      action = backendKernel.get('resource').get(
+        `@${microserviceIdentifier}:${resourceName}:${actionName}`
+      );
+
+      chai.assert.instanceOf(
+        action, Action, 'action is an instance of Action'
+      );
+
+      Object.defineProperty(Http, '@global', {
+        value: true,
+        writable: false,
+      });
+
+      let localRequestExport = RequireProxy('../../lib.compiled/Resource/LocalRequest', {
+        'superagent': HttpMock,
+      });
+
+      let LocalRequest = localRequestExport.LocalRequest;
+
+      localRequest = new LocalRequest(action, payload, method);
+
+      // complete the async
+      done();
+
+    };
+
+    KernelFactory.create({
+      Cache: Cache,
+      Security: Security,
+      Resource: Resource,
+    }, callback);
   });
 
   test('Check LOCAL_LAMBDA_ENDPOINT static getter return \'/_/lambda\'', function() {
     chai.expect(LocalRequest.LOCAL_LAMBDA_ENDPOINT).to.be.equal('/_/lambda');
   });
 
-  //test('Check _send() method for acctionType=\'lambda\'', function() {
-  //  let error = null;
-  //  let spyCallback = sinon.spy();
-  //  let actionName = 'UpdateTest';
-  //  let cache = new CacheMock();
-  //  let resource = {name: 'resourceTest', cache: cache};
-  //  let type = 'lambda';
-  //  let methods = ['GET', 'POST'];
-  //  let source = {
-  //    api: 'http://tets:8888/foo/bar?user=tj&pet=new',
-  //  };
-  //  let region = 'us-west-2';
-  //  let action = new Action(resource, actionName, type, methods, source, region);
-  //  let testRequest = new LocalRequest(action, payload, method);
-  //  testRequest.cacheImpl = cache;
-  //  testRequest.enableCache();
-  //  testRequest._native = false;
-  //  chai.expect(testRequest.isCached).to.be.equal(true);
-  //
-  //  try {
-  //    testRequest._send(spyCallback);
-  //  } catch (e) {
-  //    error = e;
-  //  }
-  //
-  //  chai.expect(error).to.be.equal(null);
-  //});
-  //
+  test('Check _send() method for acctionType="lambda"', function() {
+    let spyCallback = sinon.spy();
+
+    localRequest._send(spyCallback);
+
+
+    chai.expect(error).to.be.equal(null);
+  });
+
   //test('Check _send() method for acctionType!=\'lambda\'', function() {
   //  let error = null;
   //  let spyCallback = sinon.spy();
