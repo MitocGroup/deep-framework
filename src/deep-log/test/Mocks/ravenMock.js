6a0b15db2ce848a30d487b21710e4ff51c3183a5
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-16 15:19:41 +0200

    Add ravenMock

---
+++ src/deep-log/test/Mocks/ravenMock.js
@@ -0,0 +1,48 @@
+/**
+ * Created by vcernomschi on 11/16/15.
+ */
+
+'use strict';
+
+/**
+ * Raven Mock
+ */
+export default  {
+  Client: function(dsn) {
+    return {
+      logs: [],
+
+      captureMessage: function(message, options, callback) {
+        this.logs.push(arguments);
+
+        return {
+          message: message,
+        };
+      },
+
+      captureError: function(error, options, callback) {
+        this.logs.push(arguments);
+
+        return {
+          error: error,
+        };
+      },
+
+      captureQuery: function(query, type, callback) {
+        this.logs.push(arguments);
+
+        return {
+          query: query,
+        };
+      },
+    };
+  },
+
+  config: function() {
+
+  },
+
+  install: function() {
+
+  },
+};
