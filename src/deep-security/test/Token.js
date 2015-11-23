9b6930be7d161bcacead32eeb82382685e9000ab
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-23 14:37:23 +0200

    Add unit test for isAnonymous

--- src/deep-security/test/Token.js
+++ src/deep-security/test/Token.js
@@ -8,10 +8,12 @@ import {Token} from '../lib.compiled/Token';
 import {UserProvider} from '../lib.compiled/UserProvider';
 import {IdentityProvider} from '../lib.compiled/IdentityProvider';
 import {CredentialsManager} from '../lib.compiled/CredentialsManager';
+import {DeepResourceServiceMock} from './Mock/DeepResourceServiceMock';
+
 
 chai.use(sinonChai);
 
-suite('Token', function () {
+suite('Token', function() {
   //mocking Config
   //AWS.mock(
   //  'Config',       //the name of the AWS service that the method belongs
@@ -44,36 +46,36 @@ suite('Token', function () {
   let token = new Token(identityPoolId);
 
 
-  test('Class Token exists in Token', function () {
+  test('Class Token exists in Token', function() {
     chai.expect(typeof Token).to.equal('function');
   });
 
-  test('Check constructor sets _identityPoolId', function () {
+  test('Check constructor sets _identityPoolId', function() {
     chai.expect(token._identityPoolId).to.be.equal(identityPoolId);
   });
 
-  test('Check constructor sets _identityProvider=null', function () {
+  test('Check constructor sets _identityProvider=null', function() {
     chai.expect(token.identityProvider).to.be.equal(null);
   });
 
-  test('Check constructor sets _lambdaContext=null', function () {
+  test('Check constructor sets _lambdaContext=null', function() {
     chai.expect(token.lambdaContext).to.be.equal(null);
   });
 
-  test('Check constructor sets _user=null', function () {
+  test('Check constructor sets _user=null', function() {
     chai.expect(token._user).to.be.equal(null);
   });
 
-  test('Check constructor sets _userProvider=null', function () {
+  test('Check constructor sets _userProvider=null', function() {
     chai.expect(token._userProvider).to.be.equal(null);
   });
 
-  test('Check constructor sets _credentials=null', function () {
+  test('Check constructor sets _credentials=null', function() {
     chai.expect(token.credentials).to.be.equal(null);
   });
 
   test('Check constructor sets _credsManager as new instance ofCredentialsManager',
-    function () {
+    function() {
       chai.assert.instanceOf(
         token._credsManager, CredentialsManager, '_credsManager is an instance of CredentialsManager'
       );
@@ -81,16 +83,20 @@ suite('Token', function () {
     }
   );
 
-  //test('Check userProvider setter sets new provider', function() {
-  //  token.userProvider = null;
-  //  chai.expect(token._userProvider).to.be.equal(null);
-  //  let newUserProvider = 'userProviderTest';
-  //  token.userProvider = newUserProvider;
-  //  chai.expect(token._userProvider).to.be.equal(newUserProvider);
-  //});
+  test('Check isAnonymous getter returns true', function() {
+    chai.expect(token.isAnonymous).to.be.equal(true);
+  });
+
+  test('Check getUser() method for _isAnonymous', function() {
+    let spyCallback = sinon.spy();
+
+    token.getUser(spyCallback);
+
+    chai.expect(spyCallback).to.have.been.calledWithExactly(null);
+  });
 
   test('Check identityProvider setter',
-    function () {
+    function() {
       token.identityProvider = identityProvider;
 
       chai.expect(token.identityProvider).to.be.equal(identityProvider);
@@ -100,19 +106,50 @@ suite('Token', function () {
     }
   );
 
-  test('Check lambdaContext setter', function () {
+  test('Check lambdaContext setter', function() {
     let lambdaContext = {context: 'test context'};
     token.lambdaContext = lambdaContext;
 
     chai.expect(token.lambdaContext).to.be.equal(lambdaContext);
   });
 
-  test('Check _getRegionFromIdentityPoolId returns region', function () {
+  test('Check lambdaContext setter', function() {
+    let resourceName = 'sample';
+    let deepResourceServiceMock = new DeepResourceServiceMock();
+    let userProvider = new UserProvider(resourceName, deepResourceServiceMock);
+
+    token.userProvider = userProvider;
+
+    chai.expect(token._userProvider).to.be.equal(userProvider);
+  });
+
+  //@todo - uncomment when credentials uploaded
+  //test('Check getUser() method for !_user', function() {
+  //  let spyCallback = sinon.spy();
+  //
+  //  token.getUser(spyCallback);
+  //
+  //  chai.expect(spyCallback).to.have.been.calledWithExactly(
+  //    JSON.parse(DeepResourceServiceMock.DATA.data.Payload)
+  //  );
+  //});
+  //
+  //test('Check getUser() method for !_user', function() {
+  //  let spyCallback = sinon.spy();
+  //
+  //  token.getUser(spyCallback);
+  //
+  //  chai.expect(spyCallback).to.have.been.calledWithExactly(
+  //    JSON.parse(DeepResourceServiceMock.DATA.data.Payload)
+  //  );
+  //});
+
+  test('Check _getRegionFromIdentityPoolId returns region', function() {
     let expectedResult = identityPoolId.split(':')[0];
     chai.expect(Token.getRegionFromIdentityPoolId(identityPoolId)).to.be.equal(expectedResult);
   });
 
-  test('Check create() returns new instance of Token', function () {
+  test('Check create() returns new instance of Token', function() {
     let identityPoolId = 'us-east-1:create-a2v2-465a-877v-12fd264525ef';
 
     let actualResult = Token.create(identityPoolId);
@@ -121,7 +158,7 @@ suite('Token', function () {
     chai.expect(actualResult._identityPoolId).to.be.equal(identityPoolId);
   });
 
-  test('Check createFromIdentityProvider() returns new instance of Token', function () {
+  test('Check createFromIdentityProvider() returns new instance of Token', function() {
     let identityPoolId = 'us-east-1:provider-a2v2-465a-877v-12fd264525ef';
     identityProvider = new IdentityProvider(providers, 'amazon', userToken, userId);
 
@@ -132,7 +169,7 @@ suite('Token', function () {
     chai.expect(actualResult.identityProvider).to.be.equal(identityProvider);
   });
 
-  test('Check createFromLambdaContext() returns new instance of Token', function () {
+  test('Check createFromLambdaContext() returns new instance of Token', function() {
     let lambdaContext = {context: 'test createFromLambdaContext'};
 
     let actualResult = Token.createFromLambdaContext(identityPoolId, lambdaContext);
@@ -142,78 +179,6 @@ suite('Token', function () {
     chai.expect(actualResult.lambdaContext).to.be.equal(lambdaContext);
   });
 
-  // @note - this is goning to be refactored (https://github.com/MitocGroup/deep-framework/issues/52)
-  //test('Check getUser() method for !_user', function() {
-  //  let error = null;
-  //  let userProvider = null;
-  //  let spyCallback = sinon.spy();
-  //  let response = {
-  //    data: {
-  //      Payload: '{"message":"User loaded successfully"}',
-  //    },
-  //  };
-  //  let deepResourceServiceMock = {
-  //    get: function(name) {
-  //      return {
-  //        data: name,
-  //        request: function(id) {
-  //          return {
-  //            id: id,
-  //            send: function(callback) {
-  //              callback(response);
-  //              return;
-  //            },
-  //          };
-  //        },
-  //      };
-  //    },
-  //  };
-  //
-  //  try {
-  //    token._isAnonymous = false;
-  //    userProvider = new UserProvider(null, deepResourceServiceMock);
-  //    token.userProvider = userProvider;
-  //    token.getUser(spyCallback);
-  //  } catch (e) {
-  //    error = e;
-  //  }
-  //
-  //  chai.expect(error).to.be.equal(null);
-  //  chai.expect(spyCallback).to.have.been.calledWith(JSON.parse(response.data.Payload));
-  //});
-  //
-  //test('Check getUser() method for _user', function() {
-  //  let error = null;
-  //  let spyCallback = sinon.spy();
-  //  let user = { user: 'testUser' };
-  //
-  //  try {
-  //    token._isAnonymous = false;
-  //    token._user = user;
-  //    token.getUser(spyCallback);
-  //  } catch (e) {
-  //    error = e;
-  //  }
-  //
-  //  chai.expect(error).to.be.equal(null);
-  //  chai.expect(spyCallback).to.have.been.calledWith(user);
-  //});
-  //
-  //test('Check getUser() method for _isAnonymous', function() {
-  //  let error = null;
-  //  let spyCallback = sinon.spy();
-  //
-  //  try {
-  //    token._isAnonymous = true;
-  //    token.getUser(spyCallback);
-  //  } catch (e) {
-  //    error = e;
-  //  }
-  //
-  //  chai.expect(error).to.be.equal(null);
-  //  chai.expect(spyCallback).to.have.been.calledWith();
-  //});
-  //
   //test('Check getCredentials() method', function() {
   //  let error = null;
   //  let spyCallback = sinon.spy();
