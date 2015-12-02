ae25615657f584cb6dacf9ca13ec13625b188e2c
AlexanderC <alexander.moldova@gmail.com>
2015-12-02 16:47:59 +0200

    Put coverage folder in ignore list

---
+++ src/deep-framework/hooks/prepare_production.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+path=$(cd $(dirname $0); pwd -P)
+npm=$(which npm)
+
+assure_npm() {
+    if [ -z ${npm} ]; then
+        assure_brew
+
+        echo "Installing nodejs..."
+        ${brew} install nodejs
+
+        npm=$(which npm)
+    fi
+}
+
+assure_npm
+
+cd ${path}/../
+rm -rf ./browser
+
+# @todo: add other optimisations!
