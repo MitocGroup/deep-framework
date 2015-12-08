5281d99a21892414d281a1ffde5f9d9e3281e8c7
vcernomschi <vcernomschi@mitocgroup.com>
2015-12-08 18:47:32 +0200

    Activate send coverage

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -9,12 +9,12 @@ COVERAGE_PATH=${__SCRIPT_PATH}"/../coverage"
 istanbul-combine -d ${COVERAGE_PATH} -r lcov -p both \
   ${__SRC_PATH}/*/coverage/*.json
 
-#### Upload Coverage info to Codacy ###
-#
-#cat ${COVERAGE_PATH}"/lcov.info" | codacy-coverage
-#
-#### Cleanup! ###
-#
-#__CMD='rm -rf ./coverage'
-#
-#subpath_run_cmd ${__SRC_PATH} "$__CMD"
+### Upload Coverage info to Codacy ###
+
+cat ${COVERAGE_PATH}"/lcov.info" | codacy-coverage
+
+### Cleanup! ###
+
+__CMD='rm -rf ./coverage'
+
+subpath_run_cmd ${__SRC_PATH} "$__CMD"
