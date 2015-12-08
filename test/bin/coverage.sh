67862ca988be936b343105ab5f7a1bb1f9f8824d
vcernomschi <vcernomschi@mitocgroup.com>
2015-12-08 10:46:24 +0200

    Fix code coverage issue for codacy

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -9,12 +9,20 @@ COVERAGE_PATH=${__SCRIPT_PATH}"/../coverage"
 istanbul-combine -d ${COVERAGE_PATH} -r lcov -p none \
   ${__SRC_PATH}/*/coverage/*.json
 
+### Replacing 'lib.compiled' to 'lib'
+cd ${COVERAGE_PATH}
+SEARCH_VALUE="lib.compiled"
+REPLACE_VALUE="lib"
+export PATH_TO_TEST_TDF_FILE="$(find . -name lcov.info)"
+sed "s/${SEARCH_VALUE}/${REPLACE_VALUE}/g" "${PATH_TO_TEST_TDF_FILE}" > ${COVERAGE_PATH}/report.info
+
+
 ### Upload Coverage info to Codacy ###
 
-cat ${COVERAGE_PATH}"/lcov.info" | codacy-coverage
-
-### Cleanup! ###
-
-__CMD='rm -rf ./coverage'
-
-subpath_run_cmd ${__SRC_PATH} "$__CMD"
+#cat ${COVERAGE_PATH}"/report.info" | codacy-coverage
+#
+#### Cleanup! ###
+#
+#__CMD='rm -rf ./coverage'
+#
+#subpath_run_cmd ${__SRC_PATH} "$__CMD"
