a0acfb0d624ffe031fda654b11d586e60ed90988
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-05 12:48:29 +0200

    Add ; to test error message

--- src/deep-asset/test/Asset.js
+++ src/deep-asset/test/Asset.js
@@ -11,7 +11,7 @@ import KernelFactory from './common/KernelFactory';
 chai.use(sinonChai);
 
 suite('Asset', function() {
-  sfsdfs dzfds
+  sfsdfs dzfds;
   let assetService = null;
   let backendKernelInstance = null;
   let frontendKernelInstance = null;
