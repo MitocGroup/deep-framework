4b7f942ecddabbb26355313935714faefe4c3ab8
Vitali <Vitali>
2016-01-15 15:26:14 +0200

    Refactor deep-framework module to support Windows

---
+++ src/deep-framework/bin/test.sh
@@ -0,0 +1,8 @@
+if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
+    babel-node `which istanbul` cover --report lcovonly _mocha -- --ui tdd --recursive --reporter spec
+elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
+    echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
+else
+    echo "Running from git-bash without gathering coverage"
+    babel-node `which _mocha` --ui tdd --recursive --reporter spec
+fi
