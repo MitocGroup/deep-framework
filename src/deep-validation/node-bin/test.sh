33bc0e3fe7c9b97d8137c0c974be6d8110bdb302
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-21 11:22:14 +0200

    Update structure from bin to  node-bin

--- src/deep-validation/node-bin/test.sh
+++ src/deep-validation/node-bin/test.sh
@@ -1,5 +1,5 @@
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    babel-node `which isparta` cover --include 'lib/**/*.js' _mocha -- 'test/**/*.js' --reporter spec --ui tdd
+    babel-node `which isparta` cover --include 'lib/**/*.js' _mocha -- 'test/**/*.js' --reporter spec --ui tdd --timeout 4s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
