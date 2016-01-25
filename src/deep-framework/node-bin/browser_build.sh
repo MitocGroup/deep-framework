80a30d95f1180659aec9b3f222ccb61ba37f7d8e
AlexanderC <alexander.moldova@gmail.com>
2016-01-25 14:38:10 +0200

    Add some installation checks

--- src/deep-framework/node-bin/browser_build.sh
+++ src/deep-framework/node-bin/browser_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Skip on global installation or when fetched from registry
-if [ ! ${npm_config_global} ] && [ -d 'lib/' ]; then
+if [ ! ${npm_config_global} ] && [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
     bash scripts/browser_build.sh
 else
     echo "Skipping browser build..."
