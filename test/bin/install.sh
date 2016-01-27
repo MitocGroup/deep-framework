62788d0883028de5d5217ea15463db817082167d
AlexanderC <alexander.moldova@gmail.com>
2016-01-27 10:53:28 +0200

    Add install flags

--- test/bin/install.sh
+++ test/bin/install.sh
@@ -4,6 +4,6 @@ source $(dirname $0)/_head.sh
 
 ### Install NPM deps ###
 
-__CMD='npm install'
+__CMD='npm install --save --save-dev --save-optional --save-exact --link --no-bin-links'
 
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
