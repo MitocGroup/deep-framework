be1b7a8affdb98855eff733558d27d4a08012d02
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-04 14:58:47 +0200

    Generate missing test case

---
+++ src/deep-resource/test/Resource/Exception/ValidationError.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu Feb 04 2016 14:15:38 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {ValidationError} from '../../../lib/Resource/Exception/ValidationError';
+
+// @todo: Add more advanced tests
+suite('Resource/Exception/ValidationError', function() {
+  test('Class ValidationError exists in Resource/Exception/ValidationError', () => {
+    chai.expect(ValidationError).to.be.an('function');
+  });
+});
