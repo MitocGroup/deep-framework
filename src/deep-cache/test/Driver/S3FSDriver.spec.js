859b7635ea68d2543f5855f49fd57b640bb99878
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-08 15:09:22 +0200

    Add unit tests for AbstractFsDriverMock

--- src/deep-cache/test/Driver/S3FSDriver.spec.js
+++ src/deep-cache/test/Driver/S3FSDriver.spec.js
@@ -1,13 +1,26 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu Jan 14 2016 14:25:30 GMT+0200 (EET)
-
 'use strict';
 
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import {S3FSDriver} from '../../lib.compiled/Driver/S3FSDriver';
+import {ContainerAware} from '../../node_modules/deep-kernel/lib.compiled/ContainerAware';
 
-// @todo: Add more advanced tests
+chai.use(sinonChai);
+
 suite('Driver/S3FSDriver', () => {
+  let containerAware = new ContainerAware();
+  let s3FsDriver = new S3FSDriver(containerAware);
+
   test('Class S3FSDriver exists in Driver/S3FSDriver', () => {
     chai.expect(S3FSDriver).to.be.an('function');
+  });
+
+  test('Check constructor', () => {
+    chai.expect(s3FsDriver, 'is an instance of S3FSDriver').to.be.an.instanceOf(S3FSDriver);
+
+    chai.expect(
+      s3FsDriver._containerAware, 'is an instance of ContainerAware'
+    ).to.be.an.instanceOf(ContainerAware);
   });
 });
