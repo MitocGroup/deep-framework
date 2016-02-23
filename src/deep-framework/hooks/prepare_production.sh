960efe3439a22ed5a2939aef1567312abf71cc47
mgoria <goria.marcel@gmail.com>
2016-02-23 18:11:02 +0200

    Remove test*.js files when preparing framework from production

--- src/deep-framework/hooks/prepare_production.sh
+++ src/deep-framework/hooks/prepare_production.sh
@@ -30,6 +30,7 @@ find "${path}"/.. -type f -iname README | xargs -0 rm -rf
 
 echo "Removing tests recursively"
 find "${path}"/.. -type d -iname "test*" -print0 | xargs -0 rm -rf
+find "${path}"/.. -type f -iname "test*.js" -print0 | xargs -0 rm -rf
 
 echo "Removing empty files and directories"
 find "${path}"/.. -empty -print0 | xargs -0 rm -rf
