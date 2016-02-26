384799717a0fc6e1b47e684f535d921a1b193322
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-26 17:46:08 +0200

    Update prepare to test shell script

--- test/bin/prepare_to_test.sh
+++ test/bin/prepare_to_test.sh
@@ -5,6 +5,5 @@ source $(dirname $0)/_head.sh
 ### Find package.json files and replace main path from lib.compiled to lib###
 perl -pi -w -e 's/lib.compiled/lib/g' ${__ROOT_PATH}src/*/package.json
 
-
 ### Find .npmignore files and replace /lib to empty string###
 perl -pi -w -e 's/\/lib//g' ${__ROOT_PATH}src/*/.npmignore
