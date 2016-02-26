d2e6538291f92e938b8e59ad8f1c4b08f9cc95b5
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-26 16:16:11 +0200

    Add script to replace path for bootstrap.js files

---
+++ test/bin/replace_bootstrap_path.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source $(dirname $0)/_head.sh
+
+### Find package.json files and replace main path from lib.compiled to lib###
+
+perl -pi -w -e 's/lib.compiled/lib/g' ${__ROOT_PATH}src/*/package.json
