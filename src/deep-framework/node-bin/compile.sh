b219ae4f634b39e453a10e31bbabc61fd6c4ffe5
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-26 18:31:25 +0200

    Undo compile changes due to issues with es6

--- src/deep-framework/node-bin/compile.sh
+++ src/deep-framework/node-bin/compile.sh
@@ -1,6 +1,4 @@
-if [ "$TRAVIS" == "true" ]; then
-    echo "Skipping code transpiling to ES5 because we are in travis"
-elif [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
+if [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
    BABEL_ENV=production babel lib/ --out-dir lib.compiled/;
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
    echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
