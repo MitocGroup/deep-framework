e497475337e36722abee0ff18a895073e19b33fd
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-26 14:20:17 +0200

    Fix grammar issue

--- src/deep-resource/node-bin/compile.sh
+++ src/deep-resource/node-bin/compile.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS" == "true" ]; then
-    echo "Skipping code transpiling to ES5 becuase we are in travis"
+    echo "Skipping code transpiling to ES5 because we are in travis"
 elif [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
    BABEL_ENV=production babel lib/ --out-dir lib.compiled/;
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
