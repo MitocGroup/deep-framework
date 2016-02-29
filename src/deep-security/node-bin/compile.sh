dd50f654d5f689b8d07d6f81fabe5672f2332212
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-29 12:58:53 +0200

    Update compile scripts

--- src/deep-security/node-bin/compile.sh
+++ src/deep-security/node-bin/compile.sh
@@ -1,4 +1,6 @@
-if [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
+if [ "$TRAVIS" == "true" ] && [ "$npm_config_global" != "true" ]; then
+    echo "Skipping code transpiling to ES5 because we are in travis"
+elif [ -d 'lib/' ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
    BABEL_ENV=production babel lib/ --out-dir lib.compiled/;
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
    echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
