c582988d6d7bf18f7801e8c7393b79c33682900e
vcernomschi <vcernomschi@mitocgroup.com>
2016-03-01 16:24:31 +0200

    Update npm path usage

--- src/deep-framework/hooks/prepare_production.sh
+++ src/deep-framework/hooks/prepare_production.sh
@@ -4,13 +4,8 @@ path=$(cd $(dirname $0); pwd -P)
 npm=$(which npm)
 
 assure_npm() {
-    if [ -z ${npm} ]; then
-        assure_brew
-
-        echo "Installing nodejs..."
-        ${brew} install nodejs
-
-        npm=$(which npm)
+    if [ -z "${npm}" ]; then
+        echo "You may install NPM first!"
     fi
 }
 
