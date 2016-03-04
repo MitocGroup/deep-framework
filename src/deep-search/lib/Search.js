e95b1d8e307b917d428e74857076a94c487af6a0
mgoria <goria.marcel@gmail.com>
2016-03-04 19:12:13 +0200

    #265: Add a factory method instead of creating s specific client for each search domain

--- src/deep-search/lib/Search.js
+++ src/deep-search/lib/Search.js
@@ -6,6 +6,7 @@
 
 import Kernel from 'deep-kernel';
 import {Elasticsearch as ElasticSearchClient} from 'Client/Elasticsearch';
+import {UnknownSearchDomainException} from 'Exception/UnknownSearchDomainException';
 
 /**
  * Deep Search implementation
@@ -18,8 +19,7 @@ export class Search extends Kernel.ContainerAware {
     super();
 
     this._domains = domains;
-
-    this._rumEsClient = null;
+    this._domainClients = [];
   }
 
   /**
@@ -27,19 +27,34 @@ export class Search extends Kernel.ContainerAware {
    * @param {Function} callback
    */
   boot(kernel, callback) {
-    this._domains = kernel.esDomains;
+    this._domains = kernel.searchDomains || {};
 
     callback();
   }
 
   /**
+   * @param {String} domainName
    * @returns {ElasticSearchClient}
    */
-  get rumEsClient() {
-    if (!this._rumEsClient) {
-      this._rumEsClient = new ElasticSearchClient(this._domains.rum);
+  getClient(domainName) {
+    if (this._domainClients.indexOf(domainName) === -1) {
+      this._domainClients[domainName] = this._createClient(domainName);
     }
 
-    return this._rumEsClient;
+    return this._domainClients[domainName];
+  }
+
+  /**
+   * @param {String} domainName
+   * @returns {ElasticSearchClient}
+   * @private
+   */
+  _createClient(domainName) {
+    if (this._domains.indexOf(domainName) === -1) {
+      throw new UnknownSearchDomainException(domainName, Object.keys(this._domains));
+    }
+
+    // @todo create ES or CloudSearch client based on domainUrl
+    return new ElasticSearchClient(this._domains[domainName]);
   }
 }
