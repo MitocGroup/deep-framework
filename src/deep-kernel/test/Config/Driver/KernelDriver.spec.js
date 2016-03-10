25e2ac8b86d51ef3085d09f83d731f977fe80b3c
AlexanderC <alexander.moldova@gmail.com>
2016-03-10 14:46:43 +0200

    Generate missing tests

---
+++ src/deep-kernel/test/Config/Driver/KernelDriver.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu Mar 10 2016 14:46:23 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {KernelDriver} from '../../../lib/Config/Driver/KernelDriver';
+
+// @todo: Add more advanced tests
+suite('Config/Driver/KernelDriver', function() {
+  test('Class KernelDriver exists in Config/Driver/KernelDriver', () => {
+    chai.expect(KernelDriver).to.be.an('function');
+  });
+});
---
+++ src/deep-search/test/Search.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Thu Mar 10 2016 14:46:32 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {Search} from '../lib/Search';
+
+// @todo: Add more advanced tests
+suite('Search', function() {
+  test('Class Search exists in Search', () => {
+    chai.expect(Search).to.be.an('function');
+  });
+});
