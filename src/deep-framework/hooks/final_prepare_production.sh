e71ae8585021703e7b9d7372a1fb7af03f8d8dbf
mgoria <goria.marcel@gmail.com>
2016-03-11 16:27:18 +0200

    #277: Remove some min files

--- src/deep-framework/hooks/final_prepare_production.sh
+++ src/deep-framework/hooks/final_prepare_production.sh
@@ -12,16 +12,20 @@ assure_npm() {
 assure_npm
 
 echo "Removing shell scripts due to uselessness"
-find "${path}"/.. -type f -iname "*.sh" -print0 | xargs -0 rm -rf
+find "${path}"/../.. -type f -iname "*.sh" -print0 | xargs -0 rm -rf
 
 echo "Removing aws-sdk recursively"
-find "${path}"/.. -type d -iname "aws-sdk*" -print0 | xargs -0 rm -rf
+find "${path}"/../.. -type d -iname "aws-sdk*" -print0 | xargs -0 rm -rf
 
 echo "Removing browser libs (e.g. lodash-compat)"
 find "${path}"/../.. -type d -iname "lodash-compat*" -print0 | xargs -0 rm -rf
 
 echo "Removing grunt configuration files recursively"
-find "${path}"/.. -type f -iname "gruntfile.js" -print0 | xargs -0 rm -rf
+find "${path}"/../.. -type f -iname "gruntfile.js" -print0 | xargs -0 rm -rf
+
+# Removing some certain files
+find "${path}"/../.. -type f -ipath "*/min/locales.js" -print0 | xargs -0 rm -rf
+find "${path}"/../.. -type f -ipath "*/min/moment-with-locales.js" -print0 | xargs -0 rm -rf
 
 echo "Removing hooks directory due to uselessness"
 rm -rf "${path}"/../hooks
