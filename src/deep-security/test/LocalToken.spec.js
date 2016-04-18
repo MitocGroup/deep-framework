c031facf37e4b792d43de1f44593cc45cb12d9b6
mgoria <goria.marcel@gmail.com>
2016-04-18 16:30:39 +0300

    #326: Fix deep-security tests

--- src/deep-security/test/LocalToken.spec.js
+++ src/deep-security/test/LocalToken.spec.js
@@ -10,6 +10,7 @@ chai.use(sinonChai);
 
 suite('LocalToken', function () {
   let identityPoolId = 'us-east-1:44hgf876-a2v2-465a-877v-12fd264525ef';
+  let fakeIdentityId = 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
   let localToken = new LocalToken(identityPoolId);
   let providerName = 'amazon';
   let identityMetadata = {
@@ -43,7 +44,7 @@ suite('LocalToken', function () {
     localToken.loadCredentials(spyCallback);
 
     chai.expect(spyCallback).to.have.been.calledWithExactly(null, localToken);
-    chai.expect(localToken._credentials).to.eql({identityId: identityPoolId});
+    chai.expect(localToken._credentials).to.eql({identityId: fakeIdentityId});
   });
 
   test('Check identityProvider setter',
