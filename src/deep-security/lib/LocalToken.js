eefcccea9c84986cc77e0bbcbbb0fc7c34f1e798
mgoria <goria.marcel@gmail.com>
2016-04-18 13:50:34 +0300

    Fix security local token

--- src/deep-security/lib/LocalToken.js
+++ src/deep-security/lib/LocalToken.js
@@ -23,8 +23,8 @@ export class LocalToken extends Token {
   loadCredentials(callback = () => {}) {
     // fake local credentials
     this._credentials = {
-      // use provider user id instead of cognito identity id for local backend
-      identityId: this.identityProvider ? this.identityProvider.userId : null
+      // use provider user id or identityPoolId  instead of cognito identity id for local backend
+      identityId: this.identityProvider ? this.identityProvider.userId : this._identityPoolId
     };
 
     callback(null, this);
