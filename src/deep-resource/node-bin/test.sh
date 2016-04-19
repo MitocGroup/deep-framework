c72e4bb16911b7d2444595425bec0b942e50b23b
CCristi <ccovali@pycoding.biz>
2016-04-19 12:19:11 +0300

    # Fix travis.yml

--- src/deep-resource/node-bin/test.sh
+++ src/deep-resource/node-bin/test.sh
@@ -1,5 +1,17 @@
 if [ -d 'lib/' ] && [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    babel-node `which isparta` cover --include 'lib/**/*.js' _mocha -- 'test/**/*.spec.js' --reporter spec --ui tdd --timeout 8s
+    COMPILE_DIR='./compile';
+    [ -d ${COMPILE_DIR} ] && rm -rf ${COMPILE_DIR};
+
+    COMPILE() {
+        local resource=$1;
+        deepify ${resource} -x .js --out-dir ${COMPILE_DIR}/${resource}
+    }
+
+    COMPILE lib;
+    COMPILE test;
+
+    node `which isparta` cover --include ${COMPILE_DIR}lib/**/*.js _mocha -- ${COMPILE_DIR}/test/**/*.spec.js \
+        --reporter spec --ui tdd --timeout 4s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 elif [ -d 'lib/' ]; then
