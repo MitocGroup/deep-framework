443ef777f79ef52a5cc8dd2bfca6b8fbb4a57045
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-25 14:47:41 +0300

    Use raw coverage paths

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -8,25 +8,25 @@ source $(dirname $0)/_head.sh
 istanbul-combine -d ${__COVERAGE_PATH} -r lcovonly -p both \
   ${__SRC_PATH}/*/coverage/*.json
 
-################################################################
-### Update paths to have src/* file in coverage report       ###
-### https://github.com/codacy/node-codacy-coverage/issues/26 ###
-################################################################
-SEARCH_VALUE=$(pwd -P)"/"
-REPLACE_VALUE=""
-
-sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__COVERAGE_PATH}"/lcov.info" > ${__COVERAGE_PATH}"/coverage.info"
+#################################################################
+#### Update paths to have src/* file in coverage report       ###
+#### https://github.com/codacy/node-codacy-coverage/issues/26 ###
+#################################################################
+#SEARCH_VALUE=$(pwd -P)"/"
+#REPLACE_VALUE=""
+#
+#sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__COVERAGE_PATH}"/lcov.info" > ${__COVERAGE_PATH}"/coverage.info"
 
 
 ######################################
 ### Upload Coverage info to Codacy ###
 ######################################
-cat ${__COVERAGE_PATH}"/coverage.info" | codacy-coverage --debug
+cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage --debug
 
 #####################################################################
 ### Log top 20 file paths to be able see paths format from travis ###
 #####################################################################
-head -n 20 ${__COVERAGE_PATH}"/coverage.info"
+head -n 20 ${__COVERAGE_PATH}"/lcov.info"
 
 #############################################
 ### Cleanup! Remove all generated reports ###
