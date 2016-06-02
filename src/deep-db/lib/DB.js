5ec82626e99353e9371a6a19170268b1b883dbba
Cristi <covali.christian@gmail.com>
2016-06-02 14:56:59 +0300

    #373 Added lispCase compatibility for deep-db
    * # Added lispCase compatibility for deep-db
    
    * # Small fix

--- src/deep-db/lib/DB.js
+++ src/deep-db/lib/DB.js
@@ -60,7 +60,11 @@ export class DB extends Kernel.ContainerAware {
    */
   get(modelName) {
     if (!this.has(modelName)) {
-      throw new ModelNotFoundException(modelName);
+      modelName = this._lispCase(modelName);
+
+      if (!this.has(modelName)) {
+        throw new ModelNotFoundException(modelName);
+      }
     }
 
     let model = this._models[modelName];
@@ -300,6 +304,19 @@ export class DB extends Kernel.ContainerAware {
       tableName: this._tablesNames[name],
       schema: this._validation.getSchema(name),
     };
+  }
+
+  /**
+   * @param {String} str
+   * @returns {String}
+   * @private
+   */
+  _lispCase(str) {
+    return str
+      .replace(/([a-z])([A-Z])/g, '$1-$2')
+      .split(/[^a-z0-9\-]+/i)
+      .join('-')
+      .toLowerCase();
   }
 
   /**
