dd323945de1789ea87f5ed3c4e9a27c4d23993b5
AlexanderC <alexander.moldova@gmail.com>
2017-02-24 17:17:51 +0200

    Fix tests executable

--- src/deep-core/node-bin/test.sh
+++ src/deep-core/node-bin/test.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-bash $(dirname $0)/../../../node-bin/test.sh "$(pwd)"
+
+TEST_SCRIPT="$(dirname $0)/../../../node-bin/test.sh"
+
+[ -f "$TEST_SCRIPT" ] && bash "$TEST_SCRIPT" "$(pwd)" || echo "Skipping tests..."
