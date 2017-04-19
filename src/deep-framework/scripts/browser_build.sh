30e26a38dcca155ee572936dad48955b71f76fba
AlexanderC <alexander.moldova@gmail.com>
2017-04-19 14:28:56 +0300

    Add browser framework version

--- src/deep-framework/scripts/browser_build.sh
+++ src/deep-framework/scripts/browser_build.sh
@@ -3,4 +3,16 @@ npm install --no-bin-links \
   webpack aws-sdk lodash store \
   raven-js aws4-browser node-libs-browser \
   uglifyjs-webpack-plugin mishoo/UglifyJS2#harmony
-webpack --config webpack.browser.js --progress --color --profile --json > browser/stats.json 
+
+webpack --config webpack.browser.js --progress --color --profile --json                     >  browser/stats.json
+
+_git_name=$(git config --get user.name)
+_git_email=$(git config --get user.email)
+
+cp browser/framework.js browser/framework.js.$$
+echo -e "/**\n * Built on `date` by $_git_name <$_git_email>\n *"                           >  browser/framework.js
+npm ls deep-framework --depth 1 --silent --long false | grep deep- | awk '{print " * "$1}'  >> browser/framework.js
+npm ls --depth 1 --silent --long false | grep deep- | awk '{print " * "$2}' | tail -n+2     >> browser/framework.js
+echo -e " **/\n"                                                                            >> browser/framework.js
+cat browser/framework.js.$$                                                                 >> browser/framework.js
+rm -f browser/framework.js.$$
